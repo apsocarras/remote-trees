c7457064ba6e62f1bee6cab18b6b18b6674d8634
ChloeL6 <yenchi.le1199@gmail.com>
2022-11-03 11:30:33 -0700

    create conflict line 16

--- trees.py
+++ trees.py
@@ -1,19 +1,20 @@
 trees = ['apple', 'cedar', 'calvin', 'hooey']
 trees.extend(['cyprus', 'oak'])
 
+
 def isPalindrome(self, x: int) -> bool:
-  num_string = str(x)
-  rev_num_string = num_string[::-1]
-  if num_string == rev_num_string:
-      return True
-  else: 
-      return False
+    num_string = str(x)
+    rev_num_string = num_string[::-1]
+    if num_string == rev_num_string:
+        return True
+    else:
+        return False
 
-  # Making changes for new branch 'trees-again'.
+    # Making changes for new branch 'trees-again'.
 
 
-#Creating some conflict
-conflict_1 = 100
+# Creating some conflict
+# delete conflict_1 = 100
 conflict_2 = 200
 
 total_conflict = conflict_1 * conflict_2

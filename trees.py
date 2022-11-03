2d84942d2cc379f790a822885d22d76e00bb9cb1
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-11-03 11:22:01 -0700

    Add code to rest three way merge with conflict resolution

--- trees.py
+++ trees.py
@@ -12,8 +12,10 @@ def isPalindrome(self, x: int) -> bool:
   # Making changes for new branch 'trees-again'.
 
 
-#Creating some conflict
+# Creating some conflict
 conflict_1 = 100
 conflict_2 = 200
 
 total_conflict = conflict_1 * conflict_2
+
+# New conflict comments to have to resolve
